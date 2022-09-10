38ed120db177a59f408eed22864f4fa928e9fb88
Vasyangton <112892867+Vasyangton@users.noreply.github.com>
2022-09-11 00:37:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,12 @@ app.use(express.static(__dirname + '/public'));
 app.use('/build/', express.static(path.join(__dirname,'node_modules/three/build')));
 app.use('/jsm/',express.static(path.join(__dirname,'node_modules/three/examples/jsm')));
 
+var app = express()
+  , server = require('http').createServer(app)
+  , io = io.listen(server);
 
+app.get('/', function(req, res) {
+  res.sendfile('./public/index.html');
+});
+server.listen(80);
 
