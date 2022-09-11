ce7613e252aaafdaaa797a999a756b867dcd9fe9
Vasyangton <112892867+Vasyangton@users.noreply.github.com>
2022-09-11 18:23:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,20 +6,9 @@ app.use(express.static(__dirname + '/public'));
 app.use('/build/', express.static(path.join(__dirname,'node_modules/three/build')));
 app.use('/jsm/',express.static(path.join(__dirname,'node_modules/three/examples/jsm')));
 
+app.listen(80,() =>{
+    console.log("visit http://vasyangton.com:80");
+});
 
-const portfinder = require('portfinder');
-
-app.get('/', (req, res) => {
-  res.json({ok: 1})
-}) 
-
-async function main() {
-  const port = await portfinder.getPortPromise()
-  app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-  })
-}
-
-main()
 
 
