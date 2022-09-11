0a4f8b2024b8672b3fffa00aa61d53d5a0f630ad
Vasyangton <112892867+Vasyangton@users.noreply.github.com>
2022-09-11 17:52:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ app.use(express.static(__dirname + '/public'));
 app.use('/build/', express.static(path.join(__dirname,'node_modules/three/build')));
 app.use('/jsm/',express.static(path.join(__dirname,'node_modules/three/examples/jsm')));
 
-app.listen(10000,() =>{
-    console.log("visit http://127.0.0.1:10000");
-});
+app.listen(app.get('port'));
 
 
